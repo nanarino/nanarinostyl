18c0086c3212048271b3ef98c75f7ab89b7f3872
☘ <kogawananari@gmail.com>
2023-05-25 13:10:02 +0800

    :construction:

---
+++ snowpack.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+    mount: {
+        public: { url: '/', static: true },
+        docs: { url: '/docs' },
+        stylus: { url: '/stylus' },
+    },
+    plugins: [
+        "@marlonmarcello/snowpack-plugin-pug",
+        "snowpack-plugin-stylus"
+    ],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        // "bundle": true,
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        openUrl: "http://localhost:8080/docs/cover.html"
+    },
+    buildOptions: {
+        /* ... */
+    },
+}
--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-import vitePugPlugin from 'vite-plugin-pug-transformer'
-
-export default defineConfig({
-    plugins: [vitePugPlugin()],
-})
