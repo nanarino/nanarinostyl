d6111f9bc8d7b4b354309c8e7608030afc9d8276
☘ <kogawananari@gmail.com>
2023-06-08 20:09:31 +0800

    :+1:

--- src/mixin/menu.ts
+++ src/mixin/menu.ts
@@ -5,20 +5,30 @@ import { promisify } from "util";
 const stat = promisify(fs.stat);
 const readDir = promisify(fs.readdir);
 
+export interface page {
+    path: string;
+}
+export interface menu extends page {
+    children: page[];
+}
 
-export const getMenu = async (currentDir: string, rootDir: string = currentDir): Promise<string[]> => {
-    const urlList = [];
-    if ((await stat(path.resolve(currentDir))).isFile()) {
-        return [
-            path.relative(rootDir, currentDir)
-                .split(path.sep)
-                .join(path.posix.sep)
-                .replace(/\/(index\.mdx)|(index\.md)|(index\.astro)$/i, "")
-                .replace(/(\.mdx)|(\.md)|(\.astro)$/i, "")
-        ];
+export const getMenu = async (currentDir: string, rootDir: string = currentDir): Promise<(page | menu)[]> => {
+    const children: (page | menu)[] = [];
+    if ((await stat(path.resolve(currentDir))).isFile()) return []
+    for (const fileName of (await readDir(currentDir))) {
+        const filePath = path.join(currentDir, fileName);
+        children.push({
+            path:
+                path.relative(rootDir, filePath)
+                    .split(path.sep)
+                    .join(path.posix.sep)
+                    .replace(/\/(index\.mdx)|(index\.md)|(index\.astro)$/i, "")
+                    .replace(/(\.mdx)|(\.md)|(\.astro)$/i, "")
+            ,
+            ...((await stat(filePath)).isFile()) ? null : {
+                children: await getMenu(filePath, rootDir),
+            }
+        });
     }
-    for (const filePath of (await readDir(currentDir)).map((fileName) => path.join(currentDir, fileName))) {
-        urlList.push(...await getMenu(filePath, rootDir));
-    }
-    return urlList;
+    return children;
 }
