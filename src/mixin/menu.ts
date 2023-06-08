fe4aa72c69c91f383828da5825d452ea9778d23a
☘ <kogawananari@gmail.com>
2023-06-08 10:21:56 +0800

    :metro:  menu set

--- src/mixin/menu.ts
+++ src/mixin/menu.ts
@@ -1,24 +1,24 @@
 import fs from 'fs';
 import path from "path";
 import { promisify } from "util";
-const stat = promisify(fs.stat)
-const readDir = promisify(fs.readdir)
+
+const stat = promisify(fs.stat);
+const readDir = promisify(fs.readdir);
 
 
 export const getMenu = async (currentDir: string, rootDir: string = currentDir): Promise<string[]> => {
-    let urlList = []
+    const urlList = [];
     if ((await stat(path.resolve(currentDir))).isFile()) {
         return [
-            '/' +
             path.relative(rootDir, currentDir)
                 .split(path.sep)
                 .join(path.posix.sep)
                 .replace(/\/(index\.mdx)|(index\.md)|(index\.astro)$/i, "")
                 .replace(/(\.mdx)|(\.md)|(\.astro)$/i, "")
-        ]
+        ];
     }
-    for (let filePath of (await readDir(currentDir)).map((fileName) => path.join(currentDir, fileName))) {
-        urlList.push(...await getMenu(filePath, rootDir))
+    for (const filePath of (await readDir(currentDir)).map((fileName) => path.join(currentDir, fileName))) {
+        urlList.push(...await getMenu(filePath, rootDir));
     }
-    return urlList
+    return urlList;
 }
