7b7e59d0fc56f08ae4abc81b62c51cc2f9d13dc5
☘ <kogawananari@gmail.com>
2023-08-09 18:32:32 +0800

    :angel: support copy

---
+++ src/scripts/client/message.ts
@@ -0,0 +1,15 @@
+import prefix from "src/scripts/prefix";
+
+
+window.addEventListener(`${prefix}-message`, (event: CustomEvent) => {
+
+    const queue = document.getElementById("message-queue");
+
+    const msg = document.createElement("div");
+    
+    msg.className = `${prefix}-message`;
+    
+    msg.innerHTML = `<p class="${prefix}-paragraph" data-ellipsis="l1">${event.detail}</p>`;
+    
+    queue.appendChild(msg);
+}) 
