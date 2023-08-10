83f491b11e52740632ffe768929df8e519c736fc
☘ <kogawananari@gmail.com>
2023-08-10 18:06:49 +0800

    :art:

--- src/scripts/client/message.ts
+++ src/scripts/client/message.ts
@@ -1,15 +1,24 @@
 import prefix from "src/scripts/prefix";
+import sleep from "src/scripts/sleep";
 
+const queue = document.getElementById("message-queue");
 
-window.addEventListener(`${prefix}-message`, (event: CustomEvent<string>) => {
-
-    const queue = document.getElementById("message-queue");
+window.addEventListener(`${prefix}-message`, async (event: CustomEvent<string>) => {
 
     const msg = document.createElement("div");
-    
-    msg.className = `${prefix}-message`;
-    
-    msg.innerHTML = `<p class="${prefix}-paragraph" data-ellipsis="l1">${event.detail}</p>`;
-    
+
+    msg.style.transition = "opacity 1s, height 2s";
+    msg.innerHTML = `
+        <div class="${prefix}-message">
+            <p class="${prefix}-paragraph">${event.detail}</p>
+        </div>
+    `;
+
     queue.appendChild(msg);
-}) 
+    msg.style.height = `${msg.offsetHeight}px`;
+    await sleep(2000);
+    msg.style.opacity = '0';
+    msg.style.height = '0';
+    await sleep(2000);
+    queue.removeChild(msg);
+});
