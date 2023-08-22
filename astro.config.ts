05630e8b7d91a5b67418de3c483b8cf1e16b5db5
Jimmy <jimmyrss1102@gmail.com>
2023-08-23 03:10:47 +0800

    👷 add git page ci and change base path

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,8 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx()],
+    base: '/nanarinostyl',
     build: {
         assets: 'assets'
-    }
+    },
 });
