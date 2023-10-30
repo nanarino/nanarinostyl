4d5c6c47c32ffe06cd82524f2d50968d19e44b20
☘ <kogawananari@gmail.com>
2023-10-30 13:27:03 +0800

    :art:

--- src/scripts/server/attrs.ts
+++ src/scripts/server/attrs.ts
@@ -1,6 +1,6 @@
-export default function (dataList: Record<string, any>) {
+export default function (dataset: Record<string, any>) {
     const attrs: Record<`data-${string}`, any> = {};
-    for (const [k, v] of Object.entries(dataList)) {
+    for (const [k, v] of Object.entries(dataset)) {
         Reflect.set(attrs, `data-${k}`, v);
     }
     return attrs
