834329b5bf5296864295a8d415d8647fc932234b
☘ <kogawananari@gmail.com>
2023-10-31 12:54:25 +0800

    :memo: 优化message相关文档和使用方式

--- src/scripts/client/message.ts
+++ src/scripts/client/message.ts
@@ -4,16 +4,30 @@ import sleep from "src/scripts/sleep";
 const queue = document.getElementById("message-queue");
 
 // 全局触发
-window.addEventListener(`${prefix}-message`, async (event: CustomEvent<string>) => {
+window.addEventListener(`${prefix}-message`, async (
+    event: CustomEvent<string | {
+        content?: string
+        primary?: "success" | "danger" | "warning" | "primary"
+    }>
+) => {
 
     const msg = document.createElement("div");
+    let content = event.detail ?? "☘";
+    let primary = "";
+    if (typeof content != 'string') {
+        primary = `${content.primary ?? ""}`;
+        content = `${content.content ?? "☘"}`;
+    }
 
     msg.style.transition = "opacity 1s, height 2s";
     msg.innerHTML = `
         <div class="${prefix}-message">
-            <p class="${prefix}-paragraph">${event.detail}</p>
+            <p class="${prefix}-paragraph">${content}</p>
         </div>
     `;
+    if (primary) {
+        (msg.firstElementChild as HTMLDivElement).dataset.primary = primary;
+    }
 
     queue.appendChild(msg);
     msg.style.height = `${msg.offsetHeight}px`;
