7a549757c5e01e596d18862cf8a0781aced04956
☘ <kogawananari@gmail.com>
2024-02-01 01:27:58 +0800

    :ambulance:

--- src/scripts/client/setup.ts
+++ src/scripts/client/setup.ts
@@ -1,7 +1,7 @@
 // CSS Houdini
 if (CSS && 'paintWorklet' in CSS) {
     CSS.paintWorklet.addModule(
-        new URL(`../../assets/paint`, import.meta.url).href
+        new URL(`../../assets/paint.js`, import.meta.url).href
     );
 }
 
