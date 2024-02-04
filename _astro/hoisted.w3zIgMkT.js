65b5b4814fbb2616138e15263faf1301784e9ffe
nanarino <nanarino@users.noreply.github.com>
2024-02-04 16:23:39 +0000

    deploy: 88686975cdc11bfa8e80b5b790719e99c6b39172

---
+++ _astro/hoisted.w3zIgMkT.js
@@ -0,0 +1,25 @@
+import{p as o}from"./hoisted.ESr9dguv.js";const d=`:doodle {
+    @grid: 7 / 100%;
+    border-radius: 50%;
+}
+
+transition: .2s @r(.6s);
+@shape: clover 5;
+background: hsla(-@i(*4), 70%, 68%, @r.8);
+transform:
+    scale(@r(.2, 1.5))
+    translate(@m2.@r(±50%));
+`,s=`:doodle {
+    @grid: 8 / 100%;
+    border-radius: 50%;
+}
+
+transition: .2s @r(.6s);
+border-radius: @pick(100% 0, 0 100%);
+transform: scale(@r(.25, 1.25));
+
+background: hsla(
+    calc(240 - 6 * @x * @y),
+    70%, 68%, @r.8
+)
+`,n=document.querySelector("css-doodle"),r={light:s,dark:d};n.innerHTML=r[window.theme];window.addEventListener(`${o}-theme`,e=>{n.update(r[e.detail])});
---
+++ _astro/hoisted.x4xSgoEH.js
@@ -0,0 +1,42 @@
+import{p as a}from"./hoisted.ESr9dguv.js";const l=document.getElementById("colors-kanban");l.onclick=function(n){const e=n.target;e.tagName==="BUTTON"&&e.title&&(window.copy(`rgb(var(${e.title}))`),window.dispatchEvent(new CustomEvent(`${a}-message`,{detail:{content:`
+                        <svg
+                            class="${a}-message-prefix"
+                            data-icon="tips/outline/check-circle"
+                            data-primary="success"
+                            viewBox="0 0 48 48"
+                        >
+                            <use
+                                xlink:href="#ai:local:tips/outline/check-circle"
+                            ></use>
+                        </svg>
+                        <span>
+                            复制成功
+                        </span>
+                    `}})))};const c=["red","orange","gold","yellow","lime","green","cyan","blue","purple","pinkpurple","magenta","gray"],i=["check","close","exclamation","info"],r=document.getElementById("emit-message-kanban");r.onclick=function(n){const e=n.target;if(e.tagName==="BUTTON"){const s=Object.hasOwn(e.dataset,"primary"),o=i[Math.floor(Math.random()*i.length)];if(s){let t=Reflect.get(e.dataset,"color")||"primary";window.dispatchEvent(new CustomEvent(`${a}-message`,{detail:{content:`
+                            <svg 
+                                class="${a}-message-prefix"
+                                data-icon="tips/fill/${o}-circle-fill"
+                                viewBox="0 0 48 48"
+                            >
+                                <use
+                                    xlink:href="#ai:local:tips/fill/${o}-circle-fill"
+                                ></use>
+                            </svg>
+                            <span>
+                                发射成功
+                            </span>
+                        `,primary:s,style:{"--background-color-message":`var(--${t}-5)`,"--box-shadow-color":`var(--${t}-4)`}}})),t=c[Math.floor(Math.random()*c.length)],e.innerText=t,e.dataset.color=t,e.style.setProperty("--background-color-button",`var(--${t}-5)`),e.style.setProperty("--background-color-button-focus",`var(--${t}-6)`)}else window.dispatchEvent(new CustomEvent(`${a}-message`,{detail:{content:`
+                            <svg 
+                                class="${a}-message-prefix"
+                                data-icon="tips/outline/${o}-circle"
+                                data-primary="${{check:"success",close:"danger",exclamation:"warning",info:""}[o]}"
+                                viewBox="0 0 48 48"
+                            >
+                                <use
+                                    xlink:href="#ai:local:tips/outline/${o}-circle"
+                                ></use>
+                            </svg>
+                            <span>
+                                发射成功
+                            </span>
+                        `,primary:s}}))}};
