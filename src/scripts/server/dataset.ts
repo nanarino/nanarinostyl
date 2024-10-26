ddb69beffe140ba38e71d13eab2f06d73b93b5c3
☘ <kogawananari@gmail.com>
2024-10-26 15:48:42 +0800

    :label:

--- src/scripts/server/dataset.ts
+++ src/scripts/server/dataset.ts
@@ -1,7 +1,7 @@
 export default function (dataset: Record<string, any>) {
-    const attrs: Record<`data-${string}`, any> = {}
+    const attrs: Record<`data-${string}`, string> = {}
     for (const [k, v] of Object.entries(dataset)) {
-        Reflect.set(attrs, `data-${k}`, v)
+        Reflect.set(attrs, `data-${k}`, `${v}`)
     }
     return attrs
 }
