bc005736f67830d0501890443ed44e8c5b5ec6c4
☘ <kogawananari@gmail.com>
2025-01-10 23:27:23 +0800

    :art:

--- src/scripts/server/dataset.ts
+++ src/scripts/server/dataset.ts
@@ -1,7 +1,11 @@
 export default function (dataset: Record<string, any>) {
-    const attrs: Record<`data-${string}`, string> = {}
+    const attrs: Record<`data-${string}`, string | null> = {}
     for (const [k, v] of Object.entries(dataset)) {
-        Reflect.set(attrs, `data-${k}`, `${v}`)
+        let unbv: string | null
+        if (v === true) unbv = ""
+        else if (v === false) unbv = null
+        else unbv = `${v ?? ""}`
+        Reflect.set(attrs, `data-${k}`, unbv)
     }
     return attrs
 }
