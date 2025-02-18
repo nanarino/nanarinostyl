5e862982ac552c5da6ccba08cee489fd510075f9
☘ <kogawananari@gmail.com>
2025-02-18 17:17:18 +0800

    :fire:

--- docs/scripts/client/theme/index.ts
+++ docs/scripts/client/theme/index.ts
@@ -1,4 +1,5 @@
 import prefix from "docs/scripts/prefix"
+import toggle from "@holy-two/data-theme/dist/toggle"
 import type { Theme } from "@holy-two/data-theme"
 
 document.addEventListener("astro:after-swap", () => {
@@ -6,7 +7,8 @@ document.addEventListener("astro:after-swap", () => {
 })
 
 // 监听自定义事件 切换主题的开关时主动触发
-window.addEventListener(`${prefix}-theme`, (event?: CustomEvent<Theme>) => {
-    document.documentElement.dataset["theme"] = event.detail
-    localStorage.setItem("theme", event.detail)
+window.addEventListener(`${prefix}-theme`, (_event?: CustomEvent<Theme>) => {
+    // document.documentElement.dataset["theme"] = event.detail
+    // localStorage.setItem("theme", event.detail)
+    toggle() // 更簡單的寫法 直接切換 應該不會出問題
 })
