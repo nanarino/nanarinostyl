c50ba97e8155be558af99643f1c0c6fb555cb1ed
☘ <kogawananari@gmail.com>
2025-02-20 18:28:22 +0800

    :sparkles:

--- docs/scripts/client/setup.ts
+++ docs/scripts/client/setup.ts
@@ -11,3 +11,13 @@ if (CSS && "paintWorklet" in CSS) {
  * Web Components
  */
 import "css-doodle"
+
+/**
+ * Auto move to #anchor
+ */
+window.addEventListener("load", () => {
+    const anchor = location.hash
+    if (!anchor) return
+    const h2 = document.querySelector(anchor)
+    h2?.scrollIntoView({ behavior: "smooth" })
+})
