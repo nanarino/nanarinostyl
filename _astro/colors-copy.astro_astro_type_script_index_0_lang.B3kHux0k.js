06d529c9615bf943cc5fb45a067407b59e50cb3b
nanarino <nanarino@users.noreply.github.com>
2025-05-09 03:31:17 +0000

    deploy: 33a520325300fbb6ff1150f6df34f8e852dc197b

--- _astro/colors-copy.astro_astro_type_script_index_0_lang.B3kHux0k.js
+++ _astro/colors-copy.astro_astro_type_script_index_0_lang.B3kHux0k.js
@@ -1,4 +1,4 @@
-import{p as a}from"./prefix.DdUm4vFL.js";import{d as i}from"./dedent.BOnLu9vh.js";document.addEventListener("astro:page-load",()=>{const t=document.getElementById("colors-kanban");t&&(t.onclick=function(n){const e=n.target;e.tagName==="BUTTON"&&e.title&&(window.copy(`rgb(var(${e.title}))`),window.dispatchEvent(new CustomEvent(`${a}-message`,{detail:{content:i`
+import{p as a}from"./prefix.DdUm4vFL.js";import{d as i}from"./dedent.DVFkJXTl.js";document.addEventListener("astro:page-load",()=>{const t=document.getElementById("colors-kanban");t&&(t.onclick=function(n){const e=n.target;e.tagName==="BUTTON"&&e.title&&(window.copy(`rgb(var(${e.title}))`),window.dispatchEvent(new CustomEvent(`${a}-message`,{detail:{content:i`
                                 <svg
                                     class="${a}-message-prefix"
                                     data-icon="tips/outline/check-circle"
