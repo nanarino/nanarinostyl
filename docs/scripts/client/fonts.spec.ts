15a1afb2c18859bc878a38550fad17172c6880c1
☘ <kogawananari@gmail.com>
2025-05-21 09:52:36 +0800

    :bug:

--- docs/scripts/client/fonts.spec.ts
+++ docs/scripts/client/fonts.spec.ts
@@ -2,7 +2,7 @@
  * 測試非默認字體的效果
  */
 function test_set_fonts() {
-    if (top.location.hostname === "127.0.0.1") {
+    if (import.meta.env.DEV && top?.location?.hostname === "127.0.0.1") {
         if (!document.getElementById("test:fonts")) {
             const head = document.getElementsByTagName("head")[0]
             const link = document.createElement("link")
@@ -11,9 +11,6 @@ function test_set_fonts() {
             link.type = "text/css"
             link.href = `${import.meta.env.BASE_URL}fonts/index.css`
             link.media = "all"
-            link.onload = function () {
-                console.log(this)
-            }
             head.appendChild(link)
         }
         document.documentElement.style.setProperty(
